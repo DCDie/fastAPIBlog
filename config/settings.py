3072884cb4e3073aede80a1d3ba160a5eafc50e9
Daniel Cuznetov <daniel.cuznetov@ebs-integrator.com>
2022-12-28 00:48:14 +0200

    Add poetry

---
+++ config/settings.py
@@ -0,0 +1,12 @@
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "sqlite:///./identifier.sqlite"
+
+engine = create_async_engine(DATABASE_URL, echo=True)
+Base = declarative_base()
+async_session = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
+)
