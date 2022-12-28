32feb0a8620eb3f31779fb557fe956bc43c9dc22
Daniel Cuznetov <daniel.cuznetov@ebs-integrator.com>
2022-12-28 02:06:49 +0200

    Update engine

--- config/settings.py
+++ config/settings.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./identifier.sqlite"
+DATABASE_URL = "sqlite+aiosqlite:///database.db"
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 Base = declarative_base()
